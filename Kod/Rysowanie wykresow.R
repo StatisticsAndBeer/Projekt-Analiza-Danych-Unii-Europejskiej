8ce79952a47fa44d76113e98469a32322d1d7e26
StatisticsAndBeer <do.mieszkowski@gmail.com>
2025-05-26 16:44:02 +0200

    Skończone rysowanie wykresów pudełkowych zmiennych (wstępna analiza)

--- Kod/Rysowanie wykresow.R
+++ Kod/Rysowanie wykresow.R
@@ -3,29 +3,51 @@
 #Ograniczenie wyświetlania liczb w notacji wykładniczej
 options(scipen = 9999)
 
-#Rozzrzut punktów (dla czytelności)
+#Rozrzut punktów (dla czytelności)
 jitter <- position_jitter(width = 0.3, height = 0, seed=123)
 
 #Ustalenie wspólnego motywu
 main_plot_theme <- theme(legend.position = "none", #brak legendy
                          panel.background = element_rect(fill = "white"), #białe tło
                          panel.border = element_rect(colour = "black", fill = NA), #czrana ramka
-                         panel.grid.major.y = element_line(colour = alpha("gray", 0.4)))#szare poziome linie siatki
-                         #panel.grid.major.x = element_line(colour = alpha("black", 0.8), linetype = "dashed")) 
+                         panel.grid.major.y = element_line(colour = alpha("gray", 0.4)),#szare poziome linie siatki
+                         plot.title = element_text(hjust = 0.5)) 
 
-ggplot(data, aes(x = time, y = GDP_pc, color = geo)) +
-  geom_point(position = jitter, size=2) + 
-  geom_text_repel(mapping = aes(label=  geo), color = "black",  size = 2, 
-                  max.overlaps = 20, position = jitter, min.segment.length = 0)+
-  geom_vline(xintercept = seq(2019.5, 2022.5, b=1), linetype = "dashed") +
-  scale_y_continuous(n.breaks = 8) + 
-  main_plot_theme
+#Ustalenie nazw zmiennych 
+vars <- c("DISP_INC_hh", "EXP", "G_CONS", "G_DEBT", "GDP_pc", "HICP", "UNEMP")
+var_names <- list(geo = "Kraj",
+                  time = "Czas",
+                  DISP_INC_hh = "Skorygowany dochód rozporządzalny brutto gospodarstw domowych (moc nabywcza)",
+                  EXP = "Eksport towarów i usług [% PKB]",
+                  G_CONS = "Rządowe wydatki na konsumpcję [% PKB]",
+                  G_DEBT = "Zadłużenie rządowe [%PKB]",
+                  GDP_pc = "Przychód krajowy brutto na mieszkańca [EUR]",
+                  HICP = "Wskaźniki cen towarów i usług konsumpcyjnych",
+                  UNEMP = "Stopa bezrobocia [%]",
+                  region = "Strefa")
 
-ggplot(data, aes(x = factor(time), y = GDP_pc)) +
-  geom_boxplot(outlier.shape = NA, fill = alpha("blue", 0.1)) + 
-  geom_point(position = jitter, size=2, aes(x = factor(time), y = GDP_pc, color = geo)) + 
-  geom_text_repel(mapping = aes(label=  geo), color = "black",  size = 2, 
-                  max.overlaps = 20, position = jitter, min.segment.length = 0) +
-  scale_y_continuous(n.breaks = 8) + 
-  main_plot_theme
+# ggplot(data, aes(x = time, y = GDP_pc, color = geo)) +
+#   geom_point(position = jitter, size=2) + 
+#   geom_text_repel(mapping = aes(label=  geo), color = "black",  size = 2, 
+#                   max.overlaps = 20, position = jitter, min.segment.length = 0)+
+#   geom_vline(xintercept = seq(2019.5, 2022.5, b=1), linetype = "dashed") +
+#   scale_y_continuous(n.breaks = 8) + 
+#   main_plot_theme
+
+
+
+for(i in vars){
+  title <- var_names[[i]]
+  
+  ggplot(data, aes(x = factor(time), y = unlist(data[i]))) +
+    geom_boxplot(outlier.shape = NA, fill = alpha("blue", 0.1)) + 
+    geom_point(position = jitter, size=2, aes(x = factor(time), y = unlist(data[i]), color = geo)) + 
+    geom_text_repel(mapping = aes(label=  geo), color = "black",  size = 2, 
+                    max.overlaps = 20, position = jitter, min.segment.length = 0) +
+    scale_y_continuous(n.breaks = 8) + 
+    xlab("Czas") +
+    ylab("") + 
+    ggtitle(title) +
+    main_plot_theme
+}
 
