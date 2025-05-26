05629e315d8627e31ba74e72bbaef131c3587fc0
StatisticsAndBeer <do.mieszkowski@gmail.com>
2025-05-26 16:50:44 +0200

    Małe poprawki

--- Kod/Rysowanie wykresow.R
+++ Kod/Rysowanie wykresow.R
@@ -1,7 +1,10 @@
 ###WYKRESY======================================================================
+project_path <- "C:/Users/miesz/Desktop/Studia/Projekt-Analiza-Danych-Unii-Europejskiej"
+
 ##Estetyka----------------------------------------------------------------------
 #Ograniczenie wyświetlania liczb w notacji wykładniczej
 options(scipen = 9999)
+
 
 #Rozrzut punktów (dla czytelności)
 jitter <- position_jitter(width = 0.3, height = 0, seed=123)
@@ -16,7 +19,7 @@ main_plot_theme <- theme(legend.position = "none", #brak legendy
 #Ustalenie nazw zmiennych 
 vars <- c("DISP_INC_hh", "EXP", "G_CONS", "G_DEBT", "GDP_pc", "HICP", "UNEMP")
 var_names <- list(geo = "Kraj",
-                  time = "Czas",
+                  time = "Rok",
                   DISP_INC_hh = "Skorygowany dochód rozporządzalny brutto gospodarstw domowych (moc nabywcza)",
                   EXP = "Eksport towarów i usług [% PKB]",
                   G_CONS = "Rządowe wydatki na konsumpcję [% PKB]",
@@ -35,19 +38,24 @@ var_names <- list(geo = "Kraj",
 #   main_plot_theme
 
 
+setwd(paste(project_path, "/Wykresy/Boxploty", sep=""))
 
 for(i in vars){
   title <- var_names[[i]]
   
-  ggplot(data, aes(x = factor(time), y = unlist(data[i]))) +
+  file_name <- paste(var_names[[i]], ".png", sep="")
+  
+  p <- ggplot(data, aes(x = factor(time), y = unlist(data[i]))) +
     geom_boxplot(outlier.shape = NA, fill = alpha("blue", 0.1)) + 
     geom_point(position = jitter, size=2, aes(x = factor(time), y = unlist(data[i]), color = geo)) + 
     geom_text_repel(mapping = aes(label=  geo), color = "black",  size = 2, 
                     max.overlaps = 20, position = jitter, min.segment.length = 0) +
     scale_y_continuous(n.breaks = 8) + 
-    xlab("Czas") +
+    xlab("Rok") +
     ylab("") + 
     ggtitle(title) +
     main_plot_theme
+  
+    ggsave(filename = file_name, plot = p, height = 1600, width = 2400, units = "px")
 }
 
