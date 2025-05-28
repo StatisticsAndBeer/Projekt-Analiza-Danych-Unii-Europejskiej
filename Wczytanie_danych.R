c4ef0f9a1ce94b39a1114c16e38f11fce31343b4
daga <190594738+daga-daga@users.noreply.github.com>
2025-05-28 17:03:13 +0200

    Add files via upload

--- Wczytanie_danych.R
+++ Wczytanie_danych.R
@@ -1,6 +1,6 @@
 p<-c("eurostat", "ggplot2", "tidyr", "knitr", "tmap",
      "sf", "dplyr", "xlsx", "stringr", "purrr", "ggrepel")
-#  "skimr", "ggfortify", "lmtest", "kableExtra", 
+
 for (i in p){
   #if(!require(i, character.only = TRUE)){install.packages(i)}
   if(!i %in% installed.packages()){
@@ -18,10 +18,11 @@ CNTR_eu<-unlist(eu_countries %>%
   select(geo))
 YEARS=2019:2023
 
-#setwd("")
+project_path <- "C:\\Users\\dagma\\OneDrive\\Pulpit\\PROJEKT_EMOS\\PROJEKT"
+setwd(project_path)
 
 #Usunięcie z mapy regionów poza kontynentem europejskim
-CNTR <- st_read(dsn = "./Country Maps") %>%
+CNTR <- st_read(dsn = "./Mapy") %>%
   filter(LEVL_CODE == 2) %>%
   filter(CNTR_CODE %in% CNTR_eu) %>%
   filter(!str_detect(NUTS_ID, "FRY[0-9]+")) %>%
@@ -47,14 +48,14 @@ rm(CNTR)
 # Real GDP per capita ####
 #ewentualnie "sdg_08_10" bo jako base year używa 2020, a nie 2015
 GDP_pc<- get_eurostat(id = "tipsna40", time_format = "num",
-                         filters = list(time = YEARS, freq = "A", geo=CNTR_eu)) %>%
+                      filters = list(time = YEARS, freq = "A", geo=CNTR_eu)) %>%
   select(geo, time, values) %>%
   rename(GDP_pc = values)
 
 
 # Uneployment Rate ####
 UNEMP <- get_eurostat(id = "tps00203", time_format = "num",
-                           filters = list(time = YEARS, freq = "A", geo=CNTR_eu, unit = "PC_ACT")) %>%
+                      filters = list(time = YEARS, freq = "A", geo=CNTR_eu, unit = "PC_ACT")) %>%
   select(geo, time, values) %>%
   rename(UNEMP = values)
 
@@ -62,14 +63,14 @@ UNEMP <- get_eurostat(id = "tps00203", time_format = "num",
 
 # HICP - inflation rate
 HICP <- get_eurostat(id = "tec00118", time_format = "num",
-                           filters = list(time = YEARS, freq = "A", geo=CNTR_eu)) %>%
+                     filters = list(time = YEARS, freq = "A", geo=CNTR_eu)) %>%
   select(geo, time, values) %>%
   rename(HICP = values)
 
 
 # Governemntal consumption expenditure (Percentage of GDP) ####
 G_CONS <- get_eurostat(id = "tec00010", time_format = "num",
-                     filters = list(time = YEARS, freq = "A", geo=CNTR_eu, unit = "PC_GDP")) %>%
+                       filters = list(time = YEARS, freq = "A", geo=CNTR_eu, unit = "PC_GDP")) %>%
   select(geo, time, values) %>%
   rename(G_CONS = values)
 
@@ -77,30 +78,27 @@ G_CONS <- get_eurostat(id = "tec00010", time_format = "num",
 
 # General government gross debt - Percentage of gross domestic product (GDP) ####
 G_DEBT <- get_eurostat(id = "sdg_17_40", time_format = "num",
-                        filters = list(time = YEARS, freq = "A", geo=CNTR_eu, unit = "PC_GDP")) %>%
+                       filters = list(time = YEARS, freq = "A", geo=CNTR_eu, unit = "PC_GDP")) %>%
   select(geo, time, values) %>%
   rename(G_DEBT = values)
 
 
 # Adjusted gross disposable income of households per capita in PPS ####
 DISP_INC_hh <- get_eurostat(id = "tec00113", time_format = "num",
-                         filters = list(time = YEARS, freq = "A", geo=CNTR_eu)) %>%
+                            filters = list(time = YEARS, freq = "A", geo=CNTR_eu)) %>%
   select(geo, time, values) %>%
   rename(DISP_INC_hh = values)
 
 
-
 # Export of goods and servieces in % of GDP ####
 EXP <- get_eurostat(id = "tet00003", time_format = "num",
-                         filters = list(time = YEARS, freq = "A", geo=CNTR_eu,
-                                        na_item="P6")) %>%
+                    filters = list(time = YEARS, freq = "A", geo=CNTR_eu,
+                                   na_item="P6")) %>%
   select(geo, time, values) %>%
   rename(EXP = values)
-  
-
 
 dfs <- list(DISP_INC_hh, EXP, G_CONS, G_DEBT, GDP_pc,
-                  HICP, UNEMP)
+            HICP, UNEMP)
 data <- reduce(dfs, function(x, y) full_join(x, y, by = c("geo", "time")))
 
 rm(DISP_INC_hh, EXP, G_CONS, G_DEBT, GDP_pc,
@@ -108,18 +106,56 @@ rm(DISP_INC_hh, EXP, G_CONS, G_DEBT, GDP_pc,
 rm(dfs)
 
 
-#Przykładowy wykres
+eu_north <- c("DK", "EE", "FI", "IE", "LV", "LT", "SE")
+eu_south <- c("HR", "CY", "EL", "IT", "MT", "PT", "SI", "ES")
+eu_east  <- c("BG", "CZ", "HU", "PL", "RO", "SK")
+eu_west  <- c("AT", "BE", "FR", "DE", "LU", "NL")
 
-p <- ggplot(data) +
-  aes(x = time, y = GDP_pc, color = geo) +
-  geom_line() + 
-  geom_text(data = data %>% filter(time == 2023), aes(label = geo),
-           hjust = -0.1, show.legend = FALSE) +
-  scale_x_continuous(breaks = seq(2013, 2023, by = 2))
+data <- data %>%
+  mutate(region = case_when(
+    geo %in% eu_north ~ "North",
+    geo %in% eu_south ~ "South",
+    geo %in% eu_east  ~ "East",
+    geo %in% eu_west  ~ "West"))
 
-ggsave(filename = "Wykres.png", plot = p, width = 3200, height = 2400, units = "px")
-p
-rm(p)
+rm(eu_east, eu_south, eu_north, eu_west)
+
+data$time <- as.factor(data$time)
+data$geo <- as.factor(data$geo)
+
+
+
+# Obliczanie NA dla Bułgari w DISP_INC_hh
+# Microeconometrics (Methods and Applications) || Missing Data and Imputation 🔍
+# Cambridge University Press, 2005 may 09
+# Cameron, A. Colin; Trivedi, Pravin K
+#930-932
+
+#uzupełnianie brakującej danej
+# MODEL Z CZASEM 
+danee<-data
+danee$time <- as.numeric(as.character(danee$time))
+model_data <- danee[danee$geo == "BG", ] %>% filter(!is.na(DISP_INC_hh)) 
+
+#model dla bułgarii
+model <- lm(DISP_INC_hh ~ time, data = model_data)
+
+
+missing_rows <- danee %>%
+  filter(is.na(DISP_INC_hh) & geo == "BG") %>%
+  select(time, DISP_INC_hh)
+
+predicted <- predict(model, newdata = missing_rows)
+predicted
+#15832
+
+data$DISP_INC_hh[danee$geo == "BG" & is.na(danee$DISP_INC_hh)] <- predicted
+
+
+
+
+
+
 
 
 
