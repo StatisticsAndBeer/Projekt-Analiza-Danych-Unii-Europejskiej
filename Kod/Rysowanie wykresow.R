11123778b36fc710c7ac46630fcfd87dfc5b40c2
StatisticsAndBeer <do.mieszkowski@gmail.com>
2025-05-28 23:50:24 +0200

    Dodanie fragmentu kodu rysującego wykres krzywej Lorzenza dla poszczególnych zmiennych i lat w pliku Rysowanie Wykresów.R. Zmieniono pełne nazwy zmiennych wykorzystywanych w wykresach na takie które odpowiadają nowym wartością zmiennych EXP i G_CONS. Usunięto zbędnie importowane paczki w skrypach.

--- Kod/Rysowanie wykresow.R
+++ Kod/Rysowanie wykresow.R
@@ -1,4 +1,15 @@
 ###WYKRESY======================================================================
+p<-c("ggplot2", "tidyr", "reshape2", "dplyr", "stringr", "ineq")
+
+for (i in p){
+  #if(!require(i, character.only = TRUE)){install.packages(i)}
+  if(!i %in% installed.packages()){
+    install.packages(i)
+  }
+  library(i, character.only = TRUE)
+}
+rm(p, i)
+
 project_path <- "C:/Users/miesz/Desktop/Studia/Projekt-Analiza-Danych-Unii-Europejskiej"
 
 ##Estetyka----------------------------------------------------------------------
@@ -21,15 +32,15 @@ vars <- c("DISP_INC_hh", "EXP", "G_CONS", "G_DEBT", "GDP_pc", "HICP", "UNEMP")
 var_names <- list(geo = "Kraj",
                   time = "Rok",
                   DISP_INC_hh = "Skorygowany dochód rozporządzalny brutto gospodarstw domowych (moc nabywcza)",
-                  EXP = "Eksport towarów i usług [% PKB]",
-                  G_CONS = "Rządowe wydatki na konsumpcję [% PKB]",
+                  EXP = "Eksport towarów i usług [mln EUR (bieżące ceny)]",
+                  G_CONS = "Rządowe wydatki na konsumpcję [mln EUR (bieżące ceny)]",
                   G_DEBT = "Zadłużenie rządowe [%PKB]",
-                  GDP_pc = "Przychód krajowy brutto na mieszkańca [EUR]",
+                  GDP_pc = "Przychód krajowy brutto na mieszkańca [EUR (bieżące ceny)]",
                   HICP = "Wskaźniki cen towarów i usług konsumpcyjnych",
                   UNEMP = "Stopa bezrobocia [%]",
                   region = "Strefa")
 
-#Rysowanie----------------------------------------------------------------------
+#Wstępna analiza----------------------------------------------------------------
 #Boxploty - wstępna analiza.....................................................
 setwd(paste(project_path, "/Wykresy/Boxploty", sep=""))
 
@@ -55,7 +66,7 @@ for(i in vars){
 
 rm(i, p, file_name, title)
 
-#Histogramy - wstępna analiza...................................................
+#Gęstość - wstępna analiza......................................................
 setwd(paste(project_path, "/Wykresy/Gęstość", sep=""))
 
 
@@ -82,22 +93,24 @@ rm(i, p, file_name, title)
 
 setwd(project_path)
 
-#qq-plot - wstępna analiza......................................................
+#Badanie normalnoći-------------------------------------------------------------
+#qq-ploty.......................................................................
 library(reshape2)
 library(stringr)
+
 setwd(paste(project_path, "/Wykresy/q-q", sep=""))
 
 q_q_data <- melt(data=data, id.vars = c("time", "geo", "region"), 
-                 variable.name = "zmienna", value.name = "wartosc")
+                 variable.name = "variable", value.name = "value")
 
 file_name <- paste("Wykres q-q.png")
 title <- str_wrap("Wykresy kwantyl-kwantyl dla poszczególnych zmiennych na przestrzeni lat",
                   width = 50)
   
-p <- ggplot(q_q_data, aes(sample = wartosc)) +
+p <- ggplot(q_q_data, aes(sample = value)) +
   geom_qq_line() +
   geom_qq()+
-  facet_grid(rows = vars(zmienna), cols = vars(time), scales = "free") +
+  facet_grid(rows = vars(variable), cols = vars(time), scales = "free") +
   scale_y_continuous(n.breaks = 4) +
   ylab("Wartości rzeczywste zmiennych") +
   xlab("Teoretyczne wartości rozkładu normalnego") +
@@ -108,7 +121,47 @@ p <- ggplot(q_q_data, aes(sample = wartosc)) +
 ggsave(filename = file_name, plot = p, height = 2400, width = 1800, units = "px")
 
 
-rm(p, file_name)
+rm(p, file_name, q_q_data)
 
-setwd(project_path)
+#Badanie nierówności------------------------------------------------------------
+#Krzywa lorenza.................................................................
+library(ineq)
 
+setwd(paste(project_path, "/Wykresy/Lorenz", sep=""))
+
+#Zmiana formatu danych dla odpowiedniego rysowania wykresu
+#Inflcja pominięta ze wzglęu na ujemne wartości
+lorenz_data <- melt(data = data %>% select(-HICP), 
+                    id.vars = c("time", "geo", "region"), 
+                 variable.name = "variable", value.name = "value")
+
+lorenz_data <- lorenz_data %>%
+  group_by(variable, time) %>%
+  summarize(lorenz = list(Lc(value)$L),
+            p = list(Lc(value)$p),
+            .groups = "drop") %>%
+  unnest(c(p, lorenz)) %>%
+  rename(L = lorenz)
+
+file_name <- paste("Krzywe Lorenza.png")
+title <- str_wrap("Krzywa Lorenza obrazująca dystrybucję poszczególnych zmiennyc
+                  w karajch UE w poszczególnych latach",
+                  width = 70)
+
+p <- ggplot(lorenz_data, aes(x = p, y = L)) +
+  geom_line(color = "blue", size = 0.8) +
+  geom_ribbon(aes(ymin = L, ymax = p), fill = "blue", alpha = 0.1)+
+  geom_abline(intercept = 0, slope = 1, linetype = "dashed") +
+  facet_grid(rows = vars(variable), cols = vars(time), scales = "free") +
+  scale_y_continuous(n.breaks = 5) +
+  scale_x_continuous(n.breaks = 5) +
+  xlab("Kwantyl") +
+  ylab("Skumulowany odsetek rozpatrywanej zmiennej") +
+  ggtitle(title)+
+  main_plot_theme +
+  theme(panel.grid.major.x = element_line(colour = alpha("gray", 0.4)),
+        axis.text.x = element_text(angle = 45, hjust = 1))
+
+ggsave(filename = file_name, plot = p, height = 2400, width = 1800, units = "px")
+
+rm(file_name, title, jitter, lorenz_data, main_plot_theme, p)
